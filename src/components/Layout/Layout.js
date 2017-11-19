9f1395cee236904ed8859513a284f71881807560
Serhii <androschykserg@gmail.com>
2017-11-19 19:03:46 +0200

    Calculating the Ingredient Sum

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,3 +1,15 @@
-/**
- * Created by windows on 19.11.2017.
- */
+import  React from 'react';
+
+import Aox from '../../hoc/Aox';
+import classes from './Layout.css';
+
+const layout = ( props ) => (
+    <Aox>
+        <div>Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+    </Aox>
+);
+
+export default layout;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,20 @@
 import  React, {Component} from 'react';
 import  Aox from '../../hoc/Aox';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    };
     render () {
         return (
             <Aox>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aox>
         )
